4c834e9a0c3bd2e1b95d9bfab982a1bf1a5de1f2
Dyonovan <lookinforfun0000@gmail.com>
2016-08-03 16:32:27 -0400

    Main Class Added (No Code)

---
+++ src/main/scala/com/teambr/projectdn/ProjectDN.scala
@@ -0,0 +1,15 @@
+package com.teambr.projectdn
+
+/**
+  * This file was created for ProjectDN
+  *
+  * ProjectDN is licensed under the
+  * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+  * http://creativecommons.org/licenses/by-nc-sa/4.0/
+  *
+  * @author Dyonovan
+  * @since 8/3/2016
+  */
+object ProjectDN {
+
+}
